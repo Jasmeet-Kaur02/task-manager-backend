2f9b36f1c90e24d7289bc5d5edd83087644c223b
JasmeetKaurx <jasskaur.dha@gmail.com>
2025-03-22 20:18:42 +0530

    mongo db connection options fixes

--- app/database/index.js
+++ app/database/index.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.DATABASE_URL, {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
     console.log("Connected to the database");
   } catch (err) {
     console.log("Error while connecting to the database", err);
